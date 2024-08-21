1f817ff0097643926476073bf7a4ada581b2909f
oleg <oleg.skomoroha1@gmail.com>
2024-08-21 22:31:22 +0300

    .

---
+++ SellMarket/Model/Models/UserInfoModel.cs
@@ -0,0 +1,15 @@
+﻿namespace SellMarket.Model.Models;
+
+public class UserInfoModel
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+    public string NickName { get; set; }
+    public string UserEmail { get; set; }
+    public string PhoneNumber { get; set; }
+    
+    // address
+    // public string Region { get; set; }
+    // public string City { get; set; }
+}
