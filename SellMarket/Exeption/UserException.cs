9952c2aed6a4b0269a854987f64647327549d3f3
OlehSkmrh <oleg.skomoroha1@gmail.com>
2024-10-16 14:25:22 +0300

    commit all

---
+++ SellMarket/Exeption/UserException.cs
@@ -0,0 +1,11 @@
+﻿namespace SellMarket.Exeption
+{
+    public class UserException : Exception
+    {
+        public UserException(string message): base(message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
+
